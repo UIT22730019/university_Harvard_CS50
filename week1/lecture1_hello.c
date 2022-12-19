a52f60b2e4d58cbac52557ded99e7839c41705d3
UIT22730019 <22730019@ms.uit.edu.vn>
2022-12-20 02:11:15 +0700

    up

---
+++ week1/lecture1_hello.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+
+
+int main (void)
+{
+    // printf("Hello, world\n");
+
+    string answer = get_string("what's your name ?");
+    printf("hello, answer\n");
+}
